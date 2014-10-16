dccbebec0e97b4631966728c8e6aaf85b6048d0e
unknown <erdmanj@JohnErdman17.MaristSchool.Local>
2014-10-16 09:59:21 -0400

    Worked on java

--- CoffeeApp/gen/com/example/coffeeapp/R.java
+++ CoffeeApp/gen/com/example/coffeeapp/R.java
@@ -30,12 +30,14 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout1=0x7f090000;
-        public static final int action_settings=0x7f090013;
+        public static final int action_settings=0x7f090017;
+        public static final int btnAddDrink=0x7f090011;
         public static final int btnCoffee=0x7f090005;
         public static final int btnExpres=0x7f090007;
         public static final int btnFrap=0x7f090006;
         public static final int btnGrande=0x7f09000a;
         public static final int btnHot=0x7f090002;
+        public static final int btnResetDrink=0x7f090013;
         public static final int btnTall=0x7f090009;
         public static final int btnVenti=0x7f09000b;
         public static final int spinnerDairy=0x7f09000f;
@@ -46,8 +48,10 @@ public final class R {
         public static final int tableRow4=0x7f09000c;
         public static final int tableRow5=0x7f09000e;
         public static final int tableRow6=0x7f090010;
-        public static final int tableRow7=0x7f090011;
-        public static final int tableRow8=0x7f090012;
+        public static final int tableRow7=0x7f090012;
+        public static final int tableRow8=0x7f090014;
+        public static final int textCurrentDrink=0x7f090016;
+        public static final int textDrinksAdded=0x7f090015;
         public static final int textView1=0x7f090003;
     }
     public static final class layout {
--- CoffeeApp/src/com/example/coffeeapp/Main.java
+++ CoffeeApp/src/com/example/coffeeapp/Main.java
@@ -1,9 +1,12 @@
 package com.example.coffeeapp;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -32,8 +35,59 @@ public class Main extends Activity {
 		orders = new Orders();
 		currentDrink = new Drink ();
 		
-	}
+		//bind to XML
+		btnHot = (Button)findViewById(R.id.btnHot);
+		btnCoffee = (Button)findViewById(R.id.btnCoffee);
+		btnFrap = (Button)findViewById(R.id.btnFrap);
+		btnExpres = (Button)findViewById(R.id.btnExpres);
+		btnTall = (Button)findViewById(R.id.btnTall);
+		btnGrande = (Button)findViewById(R.id.btnGrande);
+		btnVenti = (Button)findViewById(R.id.btnVenti);
+		spinnerFlavor = (Spinner)findViewById(R.id.spinnerFlavor);
+		spinnerDairy = (Spinner)findViewById(R.id.spinnerDairy);
+		btnAddDrink = (Button)findViewById(R.id.btnAddDrink);
+		btnResetDrink = (Button)findViewById(R.id.btnResetDrink);
+		textDrinksAdded = (TextView)findViewById(R.id.textDrinksAdded);
+		textCurrentDrink = (TextView)findViewById(R.id.textCurrentDrink);
+		
+		//populate the spinner for flavor 
+		ArrayAdapter<CharSequence>flavoradapter = ArrayAdapter.createFromResource(this,R.array.flavor_array, android.R.layout.simple_spinner_dropdown_item);
+		
+		flavoradapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		spinnerFlavor.setAdapter(flavoradapter);
+		
+		
+ArrayAdapter<CharSequence>dairyadapter = ArrayAdapter.createFromResource(this,R.array.dairy_array, android.R.layout.simple_spinner_dropdown_item);
+		
+		flavoradapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		spinnerDairy.setAdapter(dairyadapter);
+		
+	}  //end on create
 
+	
+	public void btnHotClicked(View v)  {
+		if (btnHot.getText() == "Hot") {
+			btnHot.setText( "Cold") ;
+			btnHot.setBackgroundColor(Color.BLUE);
+			currentDrink.setHot(false);
+			
+		}
+		else {
+			btnHot.setText("Hot");
+			btnHot.setBackgroundColor(Color.RED);
+			currentDrink.setHot(true);
+		}
+	}
+	
+	public void coffeeClicked(View v)  {
+		currrentDrink.setType("Coffee");
+		btnCoffee.setBackgroundColor(Color.YELLOW);
+		btnFrap.setBackgroundColor(Color.LTGRAY);
+		btnExpres.setBackgroundColor(Color.LTGRAY);
+		
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -53,3 +107,4 @@ public class Main extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 }
+
