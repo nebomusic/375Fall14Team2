78a2b4ec06c66806857a53fcf0968a46a3c7592b
unknown <monarchikj@JohnMonarchik17.MaristSchool.Local>
2014-10-26 10:24:32 -0400

    Changes

--- TestingAssistant/gen/com/example/testingassistant/R.java
+++ TestingAssistant/gen/com/example/testingassistant/R.java
@@ -29,6 +29,8 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int screen_proctor=0x7f030001;
+        public static final int screen_student=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
