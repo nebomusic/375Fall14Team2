80768ce9f141a3d151280ab90ca78a66dfd3cc25
unknown <monarchikj@JohnMonarchik17.MaristSchool.Local>
2014-10-26 11:06:26 -0400

    Layout

--- TestingAssistant/gen/com/example/testingassistant/R.java
+++ TestingAssistant/gen/com/example/testingassistant/R.java
@@ -25,7 +25,38 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LinearLayout1=0x7f080000;
+        public static final int action_settings=0x7f08001f;
+        public static final int btn115=0x7f08001a;
+        public static final int btn130=0x7f08001b;
+        public static final int btn15=0x7f080016;
+        public static final int btn155=0x7f08001c;
+        public static final int btn30=0x7f080017;
+        public static final int btn55=0x7f080018;
+        public static final int btnProctor=0x7f08000a;
+        public static final int btnStandBy=0x7f08000c;
+        public static final int btnStart=0x7f08001e;
+        public static final int btnStudent=0x7f080008;
+        public static final int spinner1=0x7f080013;
+        public static final int spinner2=0x7f080014;
+        public static final int tableRow1=0x7f080001;
+        public static final int tableRow2=0x7f080003;
+        public static final int tableRow3=0x7f080005;
+        public static final int tableRow4=0x7f080007;
+        public static final int tableRow5=0x7f080009;
+        public static final int tableRow6=0x7f08000b;
+        public static final int tableRow7=0x7f080019;
+        public static final int tableRow8=0x7f08001d;
+        public static final int txtActiveStudents=0x7f08000d;
+        public static final int txtAdminTests=0x7f08000e;
+        public static final int txtEmail=0x7f080015;
+        public static final int txtFirstName=0x7f08000f;
+        public static final int txtInstructions=0x7f080006;
+        public static final int txtLastName=0x7f080010;
+        public static final int txtStudentID=0x7f080011;
+        public static final int txtTeacher=0x7f080012;
+        public static final int txtVersion=0x7f080004;
+        public static final int txtWelcome=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- TestingAssistant/src/com/example/testingassistant/MainActivity.java
+++ TestingAssistant/src/com/example/testingassistant/MainActivity.java
@@ -4,15 +4,46 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	// Hey John,
+	// Go ahead and declare the fields for all of the XML objects. To do this, Just look at the designs (UI's) that are
+	// in the testing center folder in github and follow the example below for all of the buttons, text objects, and list views.
+	// Also, go ahead and bind all of the xml objects in the on create.
+	// Good Luck!
+	// Make sure to git add, git commit, git push, and git pull!
+	
 	
 	//Put Fields Here.Break up by students, tests, Testing Center
+	
+	// Main Activity - Complete (I have already added all of the objects for the main activity)
+	
+	private TextView txtWelcome;
+	private TextView txtVersion;
+	private TextView txtInstructions;
+	private Button btnStudent;
+	private Button btnProctor;
+	private Button btnStandBy;
+	
+	//Students Class
+	
+	//Test Class
+	
+	//Testomg Center Class
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//Bind Here: 
+		btnStudent = (Button)findViewById(R.id.btnStudent);
+		txtWelcome = (TextView)findViewById(R.id.txtWelcome);
+		txtInstructions = (TextView)findViewById(R.id.txtInstructions);
+		
+		
 	}
 
 	@Override
